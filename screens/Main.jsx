c22ec56df9a9379a4dd16bf67bd7a8ffc1b5149c
Lorena Aylen <lag9512@gmail.com>
2022-09-28 17:34:29 -0300

    Se armó una estructura básica de la app. Se instaló la dependencia expo-constants, se actualizó la versión de react native y se instaló el motor hermes.

--- screens/Main.jsx
+++ screens/Main.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import Constants from 'expo-constants'
-import Home from './Home.jsx'
+import Constants from 'expo-constants';
+import Home from './Home.jsx';
+import Calculator from './Calculator.jsx'
 import {View, StyleSheet} from 'react-native';
 
 const Main = () => {
 
   return(
     <View style={styles.container}>
-      <Home/>
+      <Calculator/>
     </View>
   )
 }
@@ -15,8 +16,11 @@ const Main = () => {
 const styles = StyleSheet.create({
     container: {
       marginTop: Constants.statusBarHeight,
+      paddingHorizontal:20,
       backgroundColor: '#ffdfff',
-      flex: 1
+      flex: 1,
+      alignItems: 'stretch',
+      justifyContent: 'center'
     }
   })
 
